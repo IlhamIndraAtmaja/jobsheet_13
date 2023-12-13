a8f6c8ef81e452b56a3154a5a53ce55afea2323d
IlhamIndraAtmaja <ilhamindraatmaja98.com>
2023-12-13 19:36:27 +0700

    Pertama

---
+++ 1.py
@@ -0,0 +1,16 @@
+def guess_number():
+    secret_number = 9
+    guess_count = 0
+    guess_limit = 3
+
+    print("Tebaklah nomor rahasia")
+    while(guess_count < secret_number):
+        user =int(input("Masukan nomor = "))
+        if user == secret_number:
+             print("Anda benar")
+             break
+        else:
+            print("salah")
+            guess_count =+1
+    else:
+        print("kesempatan habis")
---
+++ 2.py
@@ -0,0 +1,10 @@
+print("Konversi berat")
+berat = int(input("Masukan berat = "))
+type = input("K atau L = ")
+
+if (type == "K"):
+    kilo = berat * 0.453592
+    print(kilo)
+elif (type == "L"):
+    lbs = berat *2.204623
+    print(lbs)
