a504500fdea33119c612abfb491259d475b17fe2
IlhamIndraAtmaja <ilhamindraatmaja98.com>
2023-12-14 07:21:57 +0700

    second

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='github',
+    version='0.1.0',
+    author='IlhamIndraAtmaja',
+    author_email='ilhamindraatmaja98.com',
+    description='Nothing',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
